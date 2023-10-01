fa03f95d30de1b9045be1b5616bb7446693a8630
Justin James <jjames@renci.org>
2023-10-01 14:25:46 -0400

    [#36] Support range header

--- src/s3/getobject.cpp
+++ src/s3/getobject.cpp
@@ -31,6 +31,8 @@
 #include <irods/rodsErrorTable.h>
 
 #include <boost/stacktrace.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
@@ -66,6 +68,50 @@ asio::awaitable<void> irods::s3::actions::handle_getobject(
     }
     std::cout << "Requested " << path << std::endl;
 
+    // read the range header if it exists
+    // Note:  We are only implementing range headers in the format range: bytes=<start>-[end]
+    std::size_t range_start = 0;
+    std::size_t range_end = 0;
+    auto range_header = parser.get().find("range");
+    if (range_header != parser.get().end()) {
+        if (range_header->value().starts_with("bytes=")) {
+            std::string range = range_header->value().substr(6);
+
+            std::vector<std::string> range_parts;
+            boost::split(range_parts, range, boost::is_any_of("-"));
+
+            if (range_parts.size() != 2) {
+                std::cerr<< "The provided range format has not been implemented." << std::endl;
+                beast::http::response<beast::http::empty_body> response;
+                response.result(beast::http::status::not_implemented);
+                beast::http::write(socket, response);
+                co_return;
+            }
+
+            try {
+                range_start = boost::lexical_cast<std::size_t>(range_parts[0]);
+                if (!range_parts[1].empty()) {
+                    range_end = boost::lexical_cast<std::size_t>(range_parts[1]);
+                }
+            }
+            catch (const boost::bad_lexical_cast&) {
+                std::cerr<< "Could not cast the start or end range to a size_t." << std::endl;
+                beast::http::response<beast::http::empty_body> response;
+                response.result(beast::http::status::not_implemented);
+                beast::http::write(socket, response);
+                co_return;
+            }
+
+        }
+        else {
+            std::cerr<< "The provided range format has not been implemented - does not begin with \"bytes=\"." << std::endl;
+            beast::http::response<beast::http::empty_body> response;
+            response.result(beast::http::status::not_implemented);
+            beast::http::write(socket, response);
+            co_return;
+        }
+    } 
+
     try {
         if (fs::client::exists(*thing, path)) {
             std::cout << "Trying to write file" << std::endl;
@@ -76,8 +122,14 @@ asio::awaitable<void> irods::s3::actions::handle_getobject(
             std::cout << "write buffer size = " << write_buffer_size << std::endl;
             std::vector<char> buf_vector(write_buffer_size);
 
+            auto file_size = irods::experimental::filesystem::client::data_object_size(*thing, path);
+            if (range_end == 0 || range_end > file_size - 1) {
+                range_end = file_size - 1;
+            }
+            auto content_length = range_end - range_start + 1;  // ranges are inclusive
+
             std::string length_field =
-                std::to_string(irods::experimental::filesystem::client::data_object_size(*thing, path));
+                std::to_string(content_length);
             response.insert(beast::http::field::content_length, length_field);
             auto md5 = irods::experimental::filesystem::client::data_object_checksum(*thing, path);
             response.insert("Content-MD5", md5);
@@ -86,6 +138,10 @@ asio::awaitable<void> irods::s3::actions::handle_getobject(
             irods::experimental::io::client::default_transport xtrans{*thing};
             irods::experimental::io::idstream d{
                 xtrans, path, irods::experimental::io::root_resource_name{irods::s3::get_resource()}};
+
+            // seek to the start range
+            d.seekg(range_start);
+            size_t offset = range_start;
 
             if (d.fail() || d.bad()) {
                 std::cout << "Fail/badbit set" << std::endl;
@@ -98,8 +154,18 @@ asio::awaitable<void> irods::s3::actions::handle_getobject(
             std::streampos current, size;
             while (d.good()) {
                 response.result(beast::http::status::ok);
-                d.read(buf_vector.data(), write_buffer_size);
+
+                // Determine the length we need to read which is the smaller
+                // of the write_buffer_size or the bytes to the end of the range.
+                // Note that ranges are inclusive which is why the +1's exist. 
+                std::size_t read_length
+                    = write_buffer_size < range_end + 1 - offset 
+                    ? write_buffer_size 
+                    : range_end + 1 - offset;
+
+                d.read(buf_vector.data(), read_length);
                 current = d.gcount();
+                offset += current;
                 size += current;
                 response.body().data = buf_vector.data();
                 response.body().size = current;
@@ -112,13 +178,19 @@ asio::awaitable<void> irods::s3::actions::handle_getobject(
                 }
                 catch (boost::system::system_error& e) {
                     if (e.code() != beast::http::error::need_buffer) {
-                        std::cout << "Not a good error!" << std::endl;
+                        std::cout << "System error when writing bytes to socket during GetObject - "
+                            << e.code().message() << "[" << e.code() << "]" << std::endl;
                         throw e;
                     }
                     else {
                         // It would be nice if we could figure out something a bit more
                         // semantic than catching an exception
                     }
+                }
+
+                // If we have now read beyond the range_end then we are done.  Break out.
+                if (offset > range_end) {
+                    break;
                 }
             }
             response.body().size = d.gcount();
