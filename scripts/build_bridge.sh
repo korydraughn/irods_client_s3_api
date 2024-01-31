898ff204573f46dd5e7a60739c870201c3cfe73e
Kory Draughn <korydraughn@renci.org>
2024-01-31 17:42:48 -0500

    [77] Remove coroutines code.

--- scripts/build_bridge.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Configure
-
-pushd irods_s3_api || exit
-mkdir build
-cd build
-cmake ..
-make package -j
--- scripts/run_bridge.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-ldconfig /irods_s3_api/build
-/irods_s3_api/build/irods_s3_api /config.json
