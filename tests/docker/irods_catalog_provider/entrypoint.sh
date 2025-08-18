15b5421a01c7f7687cee9d8bad53a67d49f57492
Alan King <alanking@renci.org>
2025-08-18 15:27:41 -0400

    [#160] Tests: Run irodsServer as foreground process
    This commit runs the irodsServer binary as the foreground process in
    the entrypoint script of the Docker image for the test Compose project.
    It also ensures that the irods-catalog-provider service can be restarted
    by only setting up the server the first time the container runs after
    being created.

--- tests/docker/irods_catalog_provider/entrypoint.sh
+++ tests/docker/irods_catalog_provider/entrypoint.sh
@@ -13,46 +13,48 @@ echo Postgres took approximately $counter seconds to fully start ...
 
 #### Set up iRODS ####
 setup_input_file=/irods_setup.input
-/var/lib/irods/scripts/setup_irods.py < ${setup_input_file}
+if [ -f ${setup_input_file} ]; then
+    /var/lib/irods/scripts/setup_irods.py < ${setup_input_file}
 
-#### Start iRODS ####
-service irods start
+    #### Start iRODS ####
+    su irods -c 'bash -c "/var/lib/irods/irodsctl start"'
 
-#### Create user1 and alice in iRODS ####
-sudo -H -u irods bash -c "iadmin mkuser user1 rodsuser"
-sudo -H -u irods bash -c "iadmin moduser user1 password user1"
-sudo -H -u irods bash -c "iadmin mkuser alice rodsuser"
-sudo -H -u irods bash -c "iadmin moduser alice password apass"
+    #### Create user1 and alice in iRODS ####
+    sudo -H -u irods bash -c "iadmin mkuser user1 rodsuser"
+    sudo -H -u irods bash -c "iadmin moduser user1 password user1"
+    sudo -H -u irods bash -c "iadmin mkuser alice rodsuser"
+    sudo -H -u irods bash -c "iadmin moduser alice password apass"
 
-#### Create newResc resource in iRODS ####
-sudo -H -u irods bash -c "iadmin mkresc newResc unixfilesystem `hostname`:/tmp/newRescVault"
+    #### Create newResc resource in iRODS ####
+    sudo -H -u irods bash -c "iadmin mkresc newResc unixfilesystem `hostname`:/tmp/newRescVault"
 
-#### Give root an environment to connect to iRODS ####
-echo 'localhost
+    #### Give root an environment to connect to iRODS ####
+    echo 'localhost
 1247
 rods
 tempZone
 rods' | iinit
 
-#### Add user1 and alice as a local user for testing ####
-useradd user1 -m
-useradd alice -m
+    #### Add user1 and alice as a local user for testing ####
+    useradd user1 -m
+    useradd alice -m
 
-#### Give alice an environment to connect to iRODS ####
-sudo -H -u alice bash -c "
+    #### Give alice an environment to connect to iRODS ####
+    sudo -H -u alice bash -c "
 echo 'localhost
 1247
 alice
 tempZone
 apass' | iinit"
 
-#### Create collections for Alice's buckets ####
-sudo -H -u alice bash -c "imkdir alice-bucket"
-sudo -H -u alice bash -c "imkdir alice-bucket2"
+    #### Create collections for Alice's buckets ####
+    sudo -H -u alice bash -c "imkdir alice-bucket"
+    sudo -H -u alice bash -c "imkdir alice-bucket2"
 
-mkdir -p /projects/irods/vsphere-testing/externals/
+    rm ${setup_input_file}
 
-cd /
+    su irods -c 'bash -c "/var/lib/irods/irodsctl stop"'
+fi
 
-#### Keep container running ####
-tail -f /dev/null
+cd /usr/sbin
+su irods -c 'bash -c "./irodsServer -u"'
